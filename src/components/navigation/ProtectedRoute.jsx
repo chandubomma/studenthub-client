a3f0931147aa0144815c70efe0e93367d9bfeae8
chandubomma <bommachandugoud@gmail.com>
2024-07-12 18:44:49 +0530

    feat - protected route

--- src/components/navigation/ProtectedRoute.jsx
+++ src/components/navigation/ProtectedRoute.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
-const ProtectedRoute = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
 
-export default ProtectedRoute
+  if (!user) {
+    return <Navigate to="/signin" />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
