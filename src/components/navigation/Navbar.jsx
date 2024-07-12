541c65a69cc3cb2ed02747b6061df46b232bd469
chandubomma <bommachandugoud@gmail.com>
2024-07-12 19:03:08 +0530

    feat - navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { user, logout } = useContext(AuthContext);
 
-export default Navbar
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between">
+        <div>
+          <Link to="/" className="mr-4">Home</Link>
+          <Link to="/guide/list" className="mr-4">Guides</Link>
+          <Link to="/about" className="mr-4">About</Link>
+        </div>
+        <div>
+          {user ? (
+            <>
+              <Link to="/account" className="mr-4">Account</Link>
+              <button onClick={logout} className="mr-4">Logout</button>
+            </>
+          ) : (
+            <>
+              <Link to="/signin" className="mr-4">Sign In</Link>
+              <Link to="/signup" className="mr-4">Sign Up</Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
