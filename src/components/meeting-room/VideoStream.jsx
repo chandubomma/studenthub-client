ddf2dff3556bfa2364cdaf0f44773ba15ee68928
chandubomma <bommachandugoud@gmail.com>
2024-07-15 18:18:05 +0530

    Video stream update

--- src/components/meeting-room/VideoStream.jsx
+++ src/components/meeting-room/VideoStream.jsx
@@ -1,24 +1,33 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+import { FaMicrophone, FaMicrophoneSlash,FaVideoSlash } from 'react-icons/fa';
 
-const VideoStream = ({ stream }) => {
-  const videoRef = useRef();
-  console.log("yes");
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.srcObject = stream;
-    }
-  }, [stream]);
-
+const VideoStream = ({ stream, isLocal, videoEnabled, audioEnabled, userName }) => {
   return (
-    <div className="w-1/2">
-      {stream ? (
-        <video ref={videoRef} autoPlay className="w-full"></video>
-      ) : (
-        <div className="bg-gray-300 flex items-center justify-center h-full">
-          <p>No Video Available</p>
+    <div className="relative w-full h-full max-h-[37rem]">
+      {
+      videoEnabled?
+      <div>
+        <video
+          className="rounded-lg shadow-lg w-full h-full"
+          ref={(video) => { if (video) video.srcObject = stream; }}
+          autoPlay
+          muted={isLocal}
+        />
+        <div className="absolute bottom-5 right-4 bg-gray-900 text-white rounded-full p-2">
+          {audioEnabled ? <FaMicrophone className='' /> : <FaMicrophoneSlash />}
         </div>
-      )}
+        <div className="absolute bottom-3 left-3 font-semibold text-white rounded-full p-3">
+          {userName}
+        </div>
+      </div>:
+        <div className="bg-gray-800 flex flex-col items-center justify-center rounded-xl w-full h-full max-h-[37rem]">
+          <span className='p-6 bg-gray-500 rounded-full mb-5'>
+            <FaVideoSlash className='text-4xl ' />
+          </span>
+          <span className="text-white text-3xl">{userName}</span>
+        </div>
+      }
+      
     </div>
   );
 };
