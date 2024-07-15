ae7d34b64200ae4ecfdef86a257ffbaf09eaed40
chandubomma <bommachandugoud@gmail.com>
2024-07-15 12:29:24 +0530

    Socket context

---
+++ src/context/SocketContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useEffect, useState, useContext } from 'react';
+import { io } from 'socket.io-client';
+import { AuthContext } from './AuthContext';
+
+export const SocketContext = createContext();
+
+const SocketProvider = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      const newSocket = io('http://localhost:3000', {
+        auth: { token: localStorage.getItem('token') },
+      });
+
+      newSocket.on('connect', () => {
+        console.log('Connected to socket server');
+      });
+
+      newSocket.on('disconnect', () => {
+        console.log('Disconnected from socket server');
+      });
+
+      setSocket(newSocket);
+
+      return () => {
+        newSocket.disconnect();
+      };
+    }
+  }, [user]);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export default SocketProvider;
