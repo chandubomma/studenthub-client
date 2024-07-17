fdbde28aa2d64aa08f8b3e5480847bac255cd0ec
chandubomma <bommachandugoud@gmail.com>
2024-07-17 21:18:17 +0530

    guides list

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -2,26 +2,20 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import GuideList from './GuideList';
+import Sidebar from '../components/navigation/Sidebar';
 
 const Guide = () => {
-  const location = useLocation();
 
-  return (
-    <div className="container mx-auto p-4 flex">
-      <aside className="w-1/4 bg-gray-100 p-4 rounded shadow-md">
-        <nav>
-          <Link to="list" className="block mb-2 text-blue-500 hover:underline">Guides List</Link>
-          <Link to="requests" className="block mb-2 text-blue-500 hover:underline">Meeting Requests</Link>
-          <Link to="meetings" className="block mb-2 text-blue-500 hover:underline">Meetings</Link>
-        </nav>
-      </aside>
-      <main className="w-3/4 p-4">
-        {location.pathname === '/guide/list' ? (
-          <GuideList/>
-        ) : (
-          <Outlet />
-        )}
-      </main>
+  return (  
+      <div className="h-screen m-0 ">
+        <div className="flex flex-row">
+            <div className="lg:flex-[2]">
+              <Sidebar/>
+            </div>
+            <div className="flex-[11] overflow-y-scroll">
+              <Outlet />
+            </div>
+        </div>
     </div>
   );
 };
--- src/pages/GuideList.jsx
+++ src/pages/GuideList.jsx
@@ -19,8 +19,8 @@ const GuideList = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Guides List</h2>
+    <div className="container mx-auto p-8">
+      <h2 className="text-xl text-gray-700 font-semibold mb-5">Guides for you</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {guides.map(guide => (
           <GuideCard key={guide.id} guide={guide} />
