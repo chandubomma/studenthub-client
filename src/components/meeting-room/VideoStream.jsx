56b53d4b0fbfeda96b8cecdd21830ee6514c4d5e
chandubomma <bommachandugoud@gmail.com>
2024-07-20 18:44:13 +0530

    videostream -audio

--- src/components/meeting-room/VideoStream.jsx
+++ src/components/meeting-room/VideoStream.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
-import { FaMicrophone, FaMicrophoneSlash,FaVideoSlash } from 'react-icons/fa';
+import { FaMicrophone, FaMicrophoneSlash, FaVideoSlash } from 'react-icons/fa';
 
-const VideoStream = ({ stream, isLocal, videoEnabled, audioEnabled, userName }) => {
+const VideoStream = ({ stream, isLocal, videoEnabled, audioEnabled, userName,profile }) => {
   return (
     <div className="relative w-full max-h-[37rem] rounded-xl">
-      {
-      videoEnabled?
       <div>
-        <video
-          className="rounded-lg shadow-lg w-full h-full"
-          ref={(video) => { if (video) video.srcObject = stream; }}
-          autoPlay
-          muted={isLocal}
-        />
-        <div className="absolute bottom-5 right-4 bg-gray-900 text-white rounded-full p-2">
-          {audioEnabled ? <FaMicrophone className='' /> : <FaMicrophoneSlash />}
-        </div>
-        <div className="absolute bottom-3 left-3 font-semibold text-white rounded-full p-3">
-          {userName}
-        </div>
-      </div>:
+        {videoEnabled ? (
+            <video
+              className="rounded-lg shadow-lg w-full h-full"
+              ref={(video) => { if (video) video.srcObject = stream; }}
+              autoPlay
+              muted={isLocal} // Mute the local video element
+            />
+        ) : (
         <div className="bg-gray-800 flex flex-col items-center justify-center rounded-xl w-full h-[37rem] max-h-[37rem]">
-          <span className='p-6 bg-gray-500 rounded-full mb-5'>
-            <FaVideoSlash className='text-4xl ' />
-          </span>
+          <img src={profile} className='w-40 h-40 rounded-full mb-5'/>
           <span className="text-white text-3xl">{userName}</span>
-        </div>
-      }
-      
+        </div>)
+        }
+        <div className="absolute bottom-5 right-4 bg-gray-900 text-white rounded-full p-2">
+            {audioEnabled ? <FaMicrophone /> : <FaMicrophoneSlash />}
+          </div>
+          {
+            videoEnabled?
+            <div className="absolute bottom-3 left-3 font-semibold text-white rounded-full p-3">
+              {userName}
+            </div>:
+            <span className='absolute bottom-5 left-4 bg-gray-900 text-white rounded-full p-2'>
+              <FaVideoSlash />
+            </span>
+          }
+          
+      </div>
+      {/* Conditionally render audio element for remote stream when video is off */}
+      {!isLocal && !videoEnabled && (
+        <audio
+          ref={(audio) => { if (audio) audio.srcObject = stream; }}
+          autoPlay
+        />
+      )}
     </div>
   );
 };
